3bd96585b8364df063c3ef17082bc5637b3948f0
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2024-05-14 16:57:39 +0700

    membuat 2 page main body

--- dicoding.js
+++ dicoding.js
@@ -11,9 +11,39 @@ const toggleMenu = () => {
 // for hidden if width is't for mobile again
 const noneMenuMobile = () => {
   const menuMobile = document.querySelector(".body-navbar-menu-mobile");
-  if (window.innerWidth > 778) {
+  if (window.innerWidth > 900) {
     menuMobile.style.display = "none";
   }
 };
 
 window.addEventListener("resize", noneMenuMobile);
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let links = document.querySelectorAll(".navbar-menu span a");
+
+//   // Memeriksa setiap tautan
+//   links.forEach(function (link) {
+//     // Menambahkan event listener untuk setiap tautan
+//     link.addEventListener("click", function () {
+//       // Menghapus kelas 'active' dari semua tautan
+//       links.forEach(function (otherLink) {
+//         otherLink.classList.remove("active");
+//       });
+
+//       // Mengubah warna tautan ketika diklik
+//       link.classList.add("active");
+
+//       // Mengecek apakah tautan telah dikunjungi
+//       if (!link.classList.contains("visited")) {
+//         // Menambahkan kelas ke tautan yang telah dikunjungi
+//         link.classList.add("visited");
+//       }
+//     });
+
+//     // Mengecek apakah tautan telah dikunjungi
+//     if (link.visited) {
+//       // Menambahkan kelas ke tautan yang telah dikunjungi
+//       link.classList.add("visited");
+//     }
+//   });
+// });
